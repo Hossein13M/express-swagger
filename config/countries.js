c422a031d281d1ffb052997c59cc39afd354f3a2
hossein <hossein13mousavi@gmail.com>
2022-01-18 12:39:38 +0330

    feat(package): add list of countries with their name and code

---
+++ config/countries.js
@@ -0,0 +1,47 @@
+export const countries = [
+  {
+    name: "USA",
+    code: 1,
+  },
+  {
+    name: "Australia",
+    code: 61,
+  },
+  {
+    name: "Argentina",
+    code: 54,
+  },
+  {
+    name: "Germany",
+    code: 49,
+  },
+  {
+    name: "Italy",
+    code: 39,
+  },
+  {
+    name: "Mexico",
+    code: 52,
+  },
+  {
+    name: "Norway",
+    code: 47,
+  },
+  {
+    name: "Serbia",
+    code: 381,
+  },
+  {
+    name: "Switzerland",
+    code: 41,
+  },
+  {
+    name: "Ukraine",
+    code: 380,
+  },
+
+  {
+    name: "Iran",
+    code: 98,
+  },
+];
