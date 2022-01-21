829f0a8e789c4d12b529ad5da90b0f40c37ed7e2
Hossein Mousavi <hossein13mousavi@gmail.com>
2022-01-21 17:49:21 +0330

    fix(routes): fix swagger documentation on comments

--- routes.js
+++ routes.js
@@ -5,14 +5,14 @@ const router = Router();
 
 /**
  * @openapi
- * /:
+ * /country:
  *   get:
- *     description: Hello World!
+ *     description: Fetch List Of Countries!
  *     responses:
  *       200:
- *         description: Returns a string.
+ *         description: Returns an array of country name and their code.
  */
-router.get("/", (req, res) => {
+router.get("", (req, res) => {
   return res.json({ countries: countries });
 });
 
